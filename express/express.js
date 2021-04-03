01b75bb140d8d14b917771e01d082bff1398a47f
Arka Sain <arka.sain@aol.com>
2021-04-03 18:12:42 +0200

    Implements parsing of incoming request bodies

--- express/express.js
+++ express/express.js
@@ -1,22 +1,44 @@
 /* Core Module Imports */
-const http = require('http');
-const path = require('path');
+const path = require("path");
 
 /* 3rd Party Imports */
-const express = require('express');
-const favicon = require('serve-favicon')
-
+const express = require("express");
+const favicon = require("serve-favicon");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+/* Resgistering a (req) body parser ... */
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/add-product', (req, res, next) => {
-    res.status(200).send('<h1>Add Products</h1>');
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+
+app.use("/add-product", (req, res, next) => {
+  res.status(200).send(
+    `
+        <html>
+            <head>
+                <title>Add Product</title>
+            </head>
+
+            <body>
+                <form action="/product" method="POST">
+                    <input type="text" name="title" placeholder="Enter product ..." />
+                    <button type="submit">Add Product</button>
+                </form>
+            </body>
+        </html>
+        `
+  );
 });
 
-app.use('/', (req, res, next) => {
-    res.status(200).send('<h1>Welcome!</h1>');
+app.post("/product", (req, res, next) => {
+  console.log("req.body: ", req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.status(200).send("<h1>Welcome!</h1>");
 });
 
 app.listen(3000);
