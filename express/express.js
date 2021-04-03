9a6d42ab4ef68b324a1b8e69aa82d9a0159376b6
Arka Sain <arka.sain@aol.com>
2021-04-03 17:40:31 +0200

    Adds favicon to the express app

--- express/express.js
+++ express/express.js
@@ -1,18 +1,24 @@
 /* Core Module Imports */
-const http = require("http");
+const http = require('http');
+const path = require('path');
 
 /* 3rd Party Imports */
 const express = require('express');
+const favicon = require('serve-favicon')
 
 
 const app = express();
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use((req, res, next) => {
     console.log('In the express middleware!');
-    next();
+    next(); // Allows the request to continue to the next middleware in line
 });
 
 app.use((req, res, next) => {
     console.log('In the second express middleware!');
+    res.status(200).send('<h1>Hello from Express!</h1>');
 });
 
 const server = http.createServer(app);
