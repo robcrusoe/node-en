0a425a112d59c6cb5bdd1878b0e5a53fbc573de4
Arka Sain <arka.sain@aol.com>
2021-04-03 22:31:43 +0200

    Implements 404 HTML template

--- express/express.js
+++ express/express.js
@@ -22,7 +22,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page not found ...</h1>');
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
   nodeTo.printMessage();
 });
 
