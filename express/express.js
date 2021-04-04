b88ced2b982f352a1ed815bb9c37175ec481eac5
Arka Sain <arka.sain@aol.com>
2021-04-04 13:15:51 +0200

    Serves static (CSS) files with express MW

--- express/express.js
+++ express/express.js
@@ -15,6 +15,7 @@ const app = express();
 
 /* Resgistering a (req) body parser ... */
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 
