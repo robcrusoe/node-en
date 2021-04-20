0c3dbb5d94b2af6a35e7b4716ca26c84a1ca8580
Arka Sain <arka.sain@aol.com>
2021-04-20 17:41:53 +0200

    Implements the MVC pattern for a `Product`

--- express/controllers/products.js
+++ express/controllers/products.js
@@ -18,14 +18,14 @@ exports.postAddProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  const products = Product.fetchAll();
-
-  res.render('./shop', {
-    prods: products,
-    docTitle: 'Shop | Node EN',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true,
+  const products = Product.fetchAll((products) => {
+    res.render('./shop', {
+      prods: products,
+      docTitle: 'Shop | Node EN',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true,
+    });
   });
 };
--- express/models/product.js
+++ express/models/product.js
@@ -1,18 +1,41 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(__dirname, '../', 'data', 'products.json');
+
+const getProductsFromFile = (callback) => {
+  fs.readFile(p, (err, data) => {
+    if (err) {
+      callback([]);
+    } else {
+      callback(JSON.parse(data));
+    }
+  });
+};
 
 module.exports = class Product {
   constructor(title) {
-      this.title = title;
+    this.title = title;
   }
 
   /* Adds the current product to the products array ... */
   save() {
+    /* Save all products to a file */
+    getProductsFromFile((products) => {
       products.push(this);
+      console.log('products [read from file]: ', products);
+
+      fs.writeFile(p, JSON.stringify(products), (error) => {
+        console.log('Error while saving to file: ', error);
+      });
+    });
   }
 
   /* Fetches all the products all at once ... */
   /* `static` allows us to directly call the method on the class itself, and not on the instantiated object ... */
-  static fetchAll() {
-    return [...products];
+  static fetchAll(callback) {
+    const p = path.join(__dirname, 'data', 'products.json');
+
+    getProductsFromFile(callback);
   }
 };
