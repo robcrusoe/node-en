fe6cf971277c0d99e668995a52c35521fce45061
Arka Sain <arka.sain@aol.com>
2021-04-28 19:49:31 +0200

    Fetches related Product(s) for current User

--- express/controllers/admin.js
+++ express/controllers/admin.js
@@ -16,20 +16,23 @@ exports.getEditProduct = (req, res, next) => {
     res.redirect('/');
   }
 
-  Product.findByPk(productId).then(product => {
-    if (!product) {
-      res.redirect('/');
-    } else {
-      res.render('./admin/edit-product', {
-        docTitle: 'Edit Product | Node EN',
-        path: '/admin/edit-product',
-        editing: editMode,
-        product: product,
-      });
-    }
-  }).catch((error) => {
-    console.log('Error while fetching Product from DB: ', error);
-  });
+  req.user
+    .getProducts({ where: { id: productId } })
+    .then((products) => {
+      if (!products) {
+        res.redirect('/');
+      } else {
+        res.render('./admin/edit-product', {
+          docTitle: 'Edit Product | Node EN',
+          path: '/admin/edit-product',
+          editing: editMode,
+          product: products[0],
+        });
+      }
+    })
+    .catch((error) => {
+      console.log('Error while fetching Product from DB: ', error);
+    });
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -40,18 +43,21 @@ exports.postEditProduct = (req, res, next) => {
   const updatedDesc = req.body.description;
   const updatedPrice = req.body.price;
 
-  Product.findByPk(prodId).then(product => {
-    product.title = updatedTitle;
-    product.price = updatedPrice;
-    product.imageUrl = updatedImageUrl;
-    product.description = updatedDesc;
-    return product.save();
-  }).then(result => {
-    console.log('Product has been updated successfully!');
-    res.redirect('/admin/products');
-  }).catch((error) => {
-    console.log('Error fetching Product from DB: ', error);
-  });
+  Product.findByPk(prodId)
+    .then((product) => {
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.imageUrl = updatedImageUrl;
+      product.description = updatedDesc;
+      return product.save();
+    })
+    .then((result) => {
+      console.log('Product has been updated successfully!');
+      res.redirect('/admin/products');
+    })
+    .catch((error) => {
+      console.log('Error fetching Product from DB: ', error);
+    });
 };
 
 exports.postAddProduct = (req, res, next) => {
@@ -59,38 +65,45 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const description = req.body.description;
   const price = req.body.price;
-  
-  req.user.createProduct({
-    title: title,
-    price: price,
-    imageUrl: imageUrl,
-    description: description
-  }).then((result) => {
-    console.log('New Product has been created!');
-    res.redirect('/products');
-  }).catch((error) => {
-    console.log('Error while saving a Product to the DB: ', error);
-  });
+
+  req.user
+    .createProduct({
+      title: title,
+      price: price,
+      imageUrl: imageUrl,
+      description: description,
+    })
+    .then((result) => {
+      console.log('New Product has been created!');
+      res.redirect('/products');
+    })
+    .catch((error) => {
+      console.log('Error while saving a Product to the DB: ', error);
+    });
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll().then((products) => {
-    res.render('./admin/products', {
-      prods: products,
-      docTitle: 'Admin Products | Node EN',
-      path: '/admin/products',
+  req.user.getProducts()
+    .then((products) => {
+      res.render('./admin/products', {
+        prods: products,
+        docTitle: 'Admin Products | Node EN',
+        path: '/admin/products',
+      });
+    })
+    .catch((error) => {
+      console.log('Error fetching Products from DB: ', error);
     });
-  }).catch((error) => {
-    console.log('Error fetching Products from DB: ', error);
-  });
 };
 
 exports.postDeleteProduct = (req, res, next) => {
   const productId = req.body.productId;
 
-  Product.destroy({ where: {id: productId} }).then(result => {
-    res.redirect('/admin/products');
-  }).catch((error) => {
-    console.log('Error while deleting Product: ', error);
-  });
+  Product.destroy({ where: { id: productId } })
+    .then((result) => {
+      res.redirect('/admin/products');
+    })
+    .catch((error) => {
+      console.log('Error while deleting Product: ', error);
+    });
 };
